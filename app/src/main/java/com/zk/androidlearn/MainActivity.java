6499d5659c6157b5849e9ebb965c28da8f46e2f2
rotoosoft <lyzkhqby@sina.com>
2016-12-13 11:43:51 +0800

    new

---
+++ app/src/main/java/com/zk/androidlearn/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zk.androidlearn;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zk.androidlearn.scroll.ScrollActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, ScrollActivity.class));
+    }
+}
